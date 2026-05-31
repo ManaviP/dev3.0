bafe6bb499edef1393788a395ed2738fbf8d4896
ManaviP <manavimunnuraya11@gmail.com>
2026-05-31 13:36:50 +0530

    small correction2

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -74,7 +74,7 @@ export default function DevHackHeroCompact() {
       {/* Scrolling Content */}
       <div className="relative z-10 mt-0 xl:-mt-[100vh]">
         {/* Initial Hero Screen Content */}
-<div className="h-[110vh] md:h-screen flex flex-col items-center xl:items-start justify-center pointer-events-none pl-0 xl:pl-24 pt-0">
+<div className="h-[104vh] md:h-screen flex flex-col items-center xl:items-start justify-center pointer-events-none pl-0 xl:pl-24 pt-0">
           <div
             className="flex flex-col items-center mt-65 md:mt-20 xl:mt-40 tall-screen-fix"
             style={{
