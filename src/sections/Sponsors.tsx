69aebb19d86b27833369c397113a9702ff6b6f7f
ManaviP <manavimunnuraya11@gmail.com>
2026-06-19 16:50:22 +0530

    added render

--- src/sections/Sponsors.tsx
+++ src/sections/Sponsors.tsx
@@ -23,7 +23,7 @@ const Sponsors: React.FC = () => {
     { name: "CodeCrafters.io", logo: "/logos/codecraft.svg" },
     { name:".xyz", logo: "/logos/xyz.png", scale: 1.2, hoverScale: 1.3 },
     { name:"n8n", logo: "/logos/n8n.png", scale: 1.2, hoverScale: 1.3 },
-    {name:"Render" , logo: "/logos/render.png", scale: 1.2, hoverScale: 1.3 },
+    {name:"Render" , logo: "/logos/render.png", scale: 1.8, hoverScale: 1.9 },
   ]
 
   const communityPartners = [
